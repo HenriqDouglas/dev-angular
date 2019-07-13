5bc320854b4a3277a005af64a1b2008a68264a39
henriqabreu <henriquedouglassa@gmail.com>
2019-07-13 00:33:49 -0400

    ApolloClient: Criando novo registro com Mutations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Apollo } from 'apollo-angular';
 import gql from 'graphql-tag';
+import { variable } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +13,7 @@ export class AppComponent {
   constructor(
     private apollo: Apollo
   ) {
+    this.createCargo();
     this.allCargos();
   }
 
@@ -28,6 +30,26 @@ export class AppComponent {
         }
       `
     }).subscribe(res => console.log('Query: ', res));
+
+  }
+
+  createCargo(): void{
+
+    this.apollo.mutate({
+      mutation: gql `
+        mutation CriarNovoCargo($nome: String!, $descricao: String, $ativo: Boolean!) {
+          novoCargo (
+            nome: $nome,
+            descricao: $descricao
+            ativo: $ativo
+            )
+        }
+      `, variables: {
+        nome: 'Func Tester',
+        descricao: 'Teste',
+        ativo: true
+      }
+    }).subscribe(res => console.log('Mutation: ', res));
 
   }
 
