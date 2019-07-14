3344e18fc9f2961f0320ff27c1ee3d260633e11c
henriqabreu <henriquedouglassa@gmail.com>
2019-07-14 14:50:32 -0400

    LoginComponent: SignIn e SignUp no mesmo Component

--- src/app/login/components/login/login.component.ts
+++ src/app/login/components/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -9,6 +9,13 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit {
 
   loginForm: FormGroup;
+  configs = {
+    isLogin: true,
+    actionText: 'SignIn',
+    buttonActionText: 'Create account'
+  };
+
+  private nameControl = new FormControl('', [Validators.required]);
 
   constructor(
     private formBuilder: FormBuilder
@@ -20,14 +27,24 @@ export class LoginComponent implements OnInit {
 
   createForm(): void {
     this.loginForm = this.formBuilder.group({
+      ativo: true,
       nome: ['', [Validators.required]],
-      descricao: [''],
-      ativo: true
+      descricao: ['']
     });
   }
 
   onSubmit(): void {
     console.log(this.loginForm.value);
   }
+
+  changeAction(): void {
+    this.configs.isLogin = !this.configs.isLogin;
+    this.configs.actionText = !this.configs.isLogin ? 'SignUp' : 'SignIn';
+    this.configs.buttonActionText = !this.configs.isLogin ? 'Already have account' : 'Create account';
+    !this.configs.isLogin ? this.loginForm.addControl('periculosidade', this.nameControl) : this.loginForm.removeControl('periculosidade');
+  }
+
+  get nome(): FormControl { return this.loginForm.get('nome') as FormControl; }
+  get periculosidade(): FormControl {return this.loginForm.get('periculosidade') as FormControl; }
 
 }
